c86860bd6a410c6bd47ad826b6f72f75803d5a89
minh195 <minh195>
2019-11-09 18:09:36 +0700

    fix button

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Oanh init Project
         </a>
       </header>
     </div>
